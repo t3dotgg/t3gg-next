3c41fab6ac7e854edafe25fa96734d2ccde015d2
theo <me@t3.gg>
2020-12-21 02:29:28 -0800

    Metadata

--- components/page.tsx
+++ components/page.tsx
@@ -12,7 +12,11 @@ export default function Page({
     <>
       <Head>
         <link rel="icon" href="/icon.png" />
-        <meta name="description" content="Theo's Site" />
+        <meta
+          name="description"
+          content="Theo Browne is a full stack engineer and React expert with years of experience working at Twitch.tv"
+        />
+        <meta name="twitter:image" content="/images/twitter.png" />
         <meta name="og:title" content={siteTitle} />
         <title>{siteTitle}</title>
       </Head>
