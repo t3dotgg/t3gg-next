bcef2465a5fee73375c65c40989e99579bdd9fef
theo <me@t3.gg>
2020-12-20 17:12:39 -0800

    cloned homepage minus animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,27 +6,44 @@ import Link from "next/link";
 import Date from "../components/date";
 import { GetStaticProps } from "next";
 
-export default function Home({
-  allPostsData,
-}: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}) {
+const HomeBody = () => (
+  <div className="home-page--content">
+    <div className="home-page--content--header">
+      <h1>Hiya</h1>
+      <p>{"I'm Theo, and I like to build things"}</p>
+      <p>
+        {"Full stack engineer at "}
+        <a href="https://twitch.tv">Twitch</a>
+        {" since January 2017"}
+      </p>
+      <p>I like skateboards, music, art, and various nonsense</p>
+      <p>
+        Check out my <a href="https://github.com/theobr">Github</a>,{" "}
+        <a href="https://twitter.com/TheoOnTwitch">Twitter</a>, and{" "}
+        <a href="https://twitch.tv/Theo">Twitch Channel</a>
+      </p>
+    </div>
+    <div className="home-page--content--footer">
+      <p>
+        You can find{" "}
+        <a href="https://github.com/TheoBr/TheoBr/blob/master/about/resume.md">
+          my resume
+        </a>{" "}
+        and{" "}
+        <a href="https://github.com/TheoBr/TheoBr/blob/master/about/projects.md">
+          my projects list
+        </a>{" "}
+        on <a href="https://github.com/TheoBr/TheoBr">Github</a>
+      </p>
+    </div>
+  </div>
+);
+
+export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Home</p>
-        <Link href={`/blog`}>
-          <a>Blog</a>
-        </Link>
-      </section>
-    </Layout>
+    <div className="home-page">
+      <HomeBody />
+    </div>
   );
 }
 
