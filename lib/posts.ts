c0c3ab4a66779a313f9257d4a7c1b310a0e1518e
theo <me@t3.gg>
2021-03-31 21:16:35 -0700

    pls

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,6 +2,12 @@ import fs from "fs";
 import path from "path";
 import { bundleMDX } from "mdx-bundler";
 
+if(process.platform === "win32"){
+  process.env.ESBUILD_BINARY_PATH = path.join(process.cwd(), 'node_modules', 'esbuild', 'esbuild.exe')
+}else{
+  process.env.ESBUILD_BINARY_PATH = path.join(process.cwd(), 'node_modules', 'esbuild', 'bin', 'esbuild')
+}
+
 const postsDirectory = path.join(process.cwd(), "posts");
 
 export type PostData = {
