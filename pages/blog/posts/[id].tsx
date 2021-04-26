6948592c2fd95b6cf8b3358e5171e1f1e9e3cc77
theo <me@t3.gg>
2021-04-25 17:39:21 -0700

    pls

--- pages/blog/posts/[id].tsx
+++ pages/blog/posts/[id].tsx
@@ -8,7 +8,8 @@ const getHostname = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_URL)
     return "https://" + process.env.NEXT_PUBLIC_VERCEL_URL;
 
-  if (process.env.NETLIFY_SITE_URL) return process.env.NETLIFY_SITE_URL;
+  if (process.env.NEXT_PUBLIC_NETLIFY_SITE_URL)
+    return process.env.NEXT_PUBLIC_NETLIFY_SITE_URL;
 
   return "http://localhost:8080";
 };
