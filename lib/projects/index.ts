ed47a9c5f63ff0d904903f9a200ee55fe0102610
theo <me@t3.gg>
2021-06-16 23:04:17 -0700

    Projects with images

---
+++ lib/projects/index.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { bundleMDX } from "mdx-bundler";
+import fs from "fs";
+
+const pathToMdx = path.join(process.cwd(), "lib/projects/projects.mdx");
+
+if (process.platform === "win32") {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    "node_modules",
+    "esbuild",
+    "esbuild.exe"
+  );
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    "node_modules",
+    "esbuild",
+    "bin",
+    "esbuild"
+  );
+}
+
+export async function getProjectsPageInfo() {
+  const fileContents = fs.readFileSync(pathToMdx, "utf8");
+  const { code, frontmatter } = await bundleMDX(fileContents);
+
+  return code;
+}
