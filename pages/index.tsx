e200c6be23c5094d1a434e8aacbdcd6115c95874
theo <me@t3.gg>
2021-09-02 02:34:15 -0700

    Fonts, branding, and cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
 import Page from "../components/page";
 import Image from "next/image";
 import Logo from "../assets/t3-block.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Page>
       <div className="bg-gray-900 bg-gradient-to-tr from-gray-900 to-gray-800">
-        <div className="flex flex-col min-h-screen animate-fade-in-down text-gray-200 relative">
-          <div className="top-0 sticky px-2 py-2 bg-gray-800 flex justify-between items-center shadow-lg z-50 w-full">
-            <Image
-              loading="eager"
-              src={Logo}
-              alt="logo"
-              width={60}
-              height={60}
-            />
+        <div className="flex flex-col items-center min-h-screen animate-fade-in-down text-gray-200 relative">
+          <div
+            className="flex flex-col justify-center items-center py-8 px-4 max-w-5xl"
+            style={{ fontFamily: "Inter" }}
+          >
+            <Image src={Logo} className="rounded-xl" />
+            <div className="text-4xl mt-4 font-medium">Welcome to T3 Tools</div>
+            <div className="my-4" />
+            <p>We're just getting started :)</p>
+            <p>
+              Started by{" "}
+              <Link href="/me">
+                <a>Theo</a>
+              </Link>
+            </p>
           </div>
-          <div className="py-4" />
-          <div className="flex flex-col p-8 max-w-5xl">Welcome to T3 Tools</div>
         </div>
       </div>
     </Page>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-in": "fade-in 0.2s ease-out",
+      },
+    },
   },
   variants: {
     extend: {},
