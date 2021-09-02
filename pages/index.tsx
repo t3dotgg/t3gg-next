80795c9ad7be8b3238190ac803d32b3ea6071951
theo <me@t3.gg>
2021-09-02 14:26:42 -0700

    Good start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,50 @@
 import Page from "../components/page";
 import Image from "next/image";
 import Logo from "../assets/t3-block.svg";
+import RoundLogo from "../assets/Round Text Logo.svg";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <Page>
-      <div className="bg-gray-900 bg-gradient-to-tr from-gray-900 to-gray-800">
-        <div className="flex flex-col items-center min-h-screen animate-fade-in-down text-gray-200 relative">
+      <div className="bg-gray-600 bg-opacity-50">
+        <div className="flex flex-col items-center min-h-screen animate-fade-in-down text-gray-800 relative">
           <div
-            className="flex flex-col justify-center items-center py-8 px-4 max-w-5xl"
+            className="flex flex-col justify-center items-center py-8 px-4 w-full shadow-xl bg-gray-900 bg-gradient-to-tr from-gray-900 to-gray-800 text-gray-200"
             style={{ fontFamily: "Inter" }}
           >
-            <Image src={Logo} className="rounded-xl" />
+            <Image src={Logo} className="rounded-xl" loading="eager" />
             <div className="text-4xl mt-4 font-medium">Welcome to T3 Tools</div>
-            <div className="my-4" />
-            <p>We're just getting started :)</p>
-            <p>
-              Started by{" "}
+            <p className="text-gray-400">
+              Built by{" "}
               <Link href="/me">
                 <a>Theo</a>
               </Link>
             </p>
+          </div>
+          <div className="pt-8" />
+
+          <div className="flex flex-col justify-center max-w-2xl px-4 md:max-w-5xl ">
+            <h2 className="text-4xl font-medium text-center">
+              We Build Things For Creators
+            </h2>
+            <p className="text-center italic">Some of them work!</p>
+            <div className="pt-8" />
+            <a href="https://round.t3.gg">
+              <div className="bg-gray-800 p-4 rounded-xl flex flex-col justify-center text-gray-200">
+                <Image
+                  src={RoundLogo}
+                  loading="eager"
+                  alt="Round - for video calls (logo)"
+                  width={200}
+                  height={60}
+                />
+                <div className="pt-4" />
+                <p className="text-center">
+                  The best way to have multiple people on your stream.
+                </p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
