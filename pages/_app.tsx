24a4b7fb2bfc2999deae23f45a7ab5158167557d
theo <me@t3.gg>
2021-09-02 23:30:26 -0700

    Add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="t3.gg">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
